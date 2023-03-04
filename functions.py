bb795ae516722beebd370efc04b5438cebf6ba8b
Apoorv pandey <apoorvpandey6@gmail.com>
2023-03-04 13:58:39 +0530

    text-analysis function

--- functions.py
+++ functions.py
@@ -21,3 +21,39 @@ from tensorflow import keras
 from sklearn import preprocessing, model_selection, feature_extraction, feature_selection, metrics, manifold, naive_bayes, pipeline
 from keras import callbacks, models, layers, preprocessing as kprocessing
 from keras import backend as K
+
+###  TEXT ANALYSIS   ###
+'''
+Plot univariate and bivariate distributions.
+'''
+def plot_distributions(dtf, x, max_cat=20, top=None, y=None, bins=None, figsize=(10,5)):
+    ## univariate
+    if y is None:
+        fig, ax = plt.subplots(figsize=figsize)
+        fig.suptitle(x, fontsize=15)
+        ### categorical
+        if dtf[x].nunique() <= max_cat:
+            if top is None:
+                dtf[x].reset_index().groupby(x).count().sort_values(by="index").plot(kind="barh", legend=False, ax=ax).grid(axis='x')
+            else:   
+                dtf[x].reset_index().groupby(x).count().sort_values(by="index").tail(top).plot(kind="barh", legend=False, ax=ax).grid(axis='x')
+            ax.set(ylabel=None)
+        ### numerical
+        else:
+            sns.distplot(dtf[x], hist=True, kde=True, kde_kws={"shade":True}, ax=ax)
+            ax.grid(True)
+            ax.set(xlabel=None, yticklabels=[], yticks=[])
+
+    ## bivariate
+    else:
+        fig, ax = plt.subplots(nrows=1, ncols=2, sharex=False, sharey=False, figsize=figsize)
+        fig.suptitle(x, fontsize=15)
+        for i in dtf[y].unique():
+            sns.distplot(dtf[dtf[y]==i][x], hist=True, kde=False, bins=bins, hist_kws={"alpha":0.8}, axlabel="", ax=ax[0])
+            sns.distplot(dtf[dtf[y]==i][x], hist=False, kde=True, kde_kws={"shade":True}, axlabel="", ax=ax[1])
+        ax[0].set(title="histogram")
+        ax[0].grid(True)
+        ax[0].legend(dtf[y].unique())
+        ax[1].set(title="density")
+        ax[1].grid(True)
+    plt.show()
